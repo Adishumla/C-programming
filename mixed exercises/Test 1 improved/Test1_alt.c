ef5a8c9a546de8f4fd3d13746b8a153efe8c76ac
adishumla <Adam.garali@gmail.com>
2020-09-22 23:13:48 +0200

    .

--- mixed exercises/Test 1 improved/Test1_alt.c
+++ mixed exercises/Test 1 improved/Test1_alt.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include <stdlib.h> // Neded for system("Pause"); (wont work with out it, only works on windowns for other systems use: printf("Press enter to continue...\n"); getchar(); )
 
 int main()
 {
     char fullName[25], address[25], postalAdress[25];
 
-    printf("What is your full name?\n");
-    gets(fullName);
+    printf("What is your full name?\n"); // Asks for users full name.
+    gets(fullName);                      // Stores users full name to be used later.
 
     printf("\nWhat is your address?\n");
     gets(address);
@@ -13,7 +14,8 @@ int main()
     printf("\nWhat is your postaladdress?\n");
     gets(postalAdress);
     
-    printf("\n%s from %s in %s", fullName, address, postalAdress);
+    printf("\n%s from %s in %s\n\n", fullName, address, postalAdress); // Writes the users full name, address and postaladdress.
+    system("Pause"); // Pauses system so the user can see if the information was correct.
 
     return 0;
 }
