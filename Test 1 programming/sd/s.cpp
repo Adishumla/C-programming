c7f80fddef6e79ac619db4467dc7207db70eedb1
adishumla <adam.garali@gmail.com>
2021-03-15 13:07:05 +0100

    La till kommentar

---
+++ Test 1 programming/sd/s.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+FILE *f;
+ int buf[6],i,tal;
+ for (i=1;i<=3;i++)
+ {
+ printf("Skriv in tal %i\n ",i);
+ scanf("%i",&tal);
+ buf[i]=tal;
+ }
+ f = fopen("C:\\filtest\\uppg5.txt","w");
+ for (i=1;i<=3;i++)
+ {
+ fprintf(f,"%i\n", buf[i]);
+ }
+ fclose(f);
+ f = fopen("C:\\filtest\\uppg5.txt","r");
+ for (i=1;i<=3;i++)
+ {
+ fscanf(f,"%d",&buf[i]);
+ }
+ for (i=3;i>=1;i--)
+ {
+ printf("%d\n",buf[i]);
+ }
+ fclose(f);
+ system("PAUSE"); //Detta pausar programmet...
+ return 0;
+ return 0;
+}
